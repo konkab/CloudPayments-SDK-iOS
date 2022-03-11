e5c039fe290fd0c3672dd5aeba9fdaf22124a169
Konstantin Kabanov <konkab@icloud.com>
2022-03-11 19:11:21 +0700

    Fix bundle

--- Source/Utils/Extenstions/Bundle+Extenstions.swift
+++ Source/Utils/Extenstions/Bundle+Extenstions.swift
@@ -10,6 +10,33 @@ import Foundation
 
 extension Bundle {
     class var mainSdk: Bundle {
-        return Bundle.module
+        var ourBundle: Bundle?
+        #if SWIFT_PACKAGE
+            ourBundle = Bundle.module
+        #endif
+
+        if ourBundle == nil {
+            ourBundle = Bundle(path: "CloudPayments.bundle")
+        }
+
+        if ourBundle == nil {
+            // This might be the same as the previous check if not using a dynamic framework
+            if let path = Bundle(for: PaymentForm.self).path(
+                forResource: "CloudPayments", ofType: "bundle")
+            {
+                ourBundle = Bundle(path: path)
+            }
+        }
+
+        if ourBundle == nil {
+            // This will be the same as mainBundle if not using a dynamic framework
+            ourBundle = Bundle(for: PaymentForm.self)
+        }
+
+        if let ourBundle = ourBundle {
+            return ourBundle
+        } else {
+            return Bundle.main
+        }
     }
 }
